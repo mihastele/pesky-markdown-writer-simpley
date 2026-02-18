14b453fb03854a542f852e7a9d654373ba3fad30
Miha Stele <stele1000@gmail.com>
2026-02-18 12:26:45 +0100

    Revert "Update"
    This reverts commit f953a10276c95cb06ef7165fb88dfc3a5453c1b3.

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,9 +4,9 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "data/schema.prisma",
+  schema: "prisma/schema.prisma",
   migrations: {
-    path: "data/migrations",
+    path: "prisma/migrations",
   },
   datasource: {
     url: process.env["DATABASE_URL"],
